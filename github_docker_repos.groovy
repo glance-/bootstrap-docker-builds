d58680b7db6ab58850ed032ad3ab70c3c854cb4e
Leif Johansson <leifj@sunet.se>
2016-06-01 00:23:40 +0200

    dump link

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -7,7 +7,7 @@ def org = 'SUNET'
 def url = "https://api.github.com/"
 def api = new HTTPBuilder(url)
 def next_path = "/orgs/${org}/repos"
-while (url) {
+while (next_path != null) {
   api.request(GET,JSON) { req ->
     uri.path = next_path
     headers.'User-Agent' = 'Mozilla/5.0'
@@ -17,11 +17,12 @@ while (url) {
     assert resp.status == 200
 
     def repos = reader
+    next_path = null
     resp.headers.'Link'.split(',').each {
        out.println(it)
        def m = (it =~ /<https:\/\/api.github.com(\s+)>\S+rel=\"next\"/)
        if (m.matches()) {
-          next_path = m.group(0)
+          next_path = m.group(1)
        }
     }
 
