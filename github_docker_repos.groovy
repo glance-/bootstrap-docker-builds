b00a652b8dd92538d11a72c455d564aa8737d28e
Leif Johansson <leifj@sunet.se>
2016-06-02 23:12:02 +0200

    never mind

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -50,8 +50,8 @@ while (next_path != null) {
       hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
       if (name.contains("docker-satosa")) {
          job(name) {
-            def env = System.getenv()
-            env << ['slack':['room':'devops']]
+            def environ = System.getenv()
+            def env = ['slack':['room':'devops']]
             def files = workspace.list()
             if (files.contains('.jenkins.json')) {
                env << (new JsonSlurper()).parseText(streamFileFromWorkspace('.jenkins.json'))
@@ -71,7 +71,7 @@ while (next_path != null) {
             publishers {
                slackNotifier {
                   teamDomain('SUNET')
-                  authToken(env['SLACK_TOKEN'])
+                  authToken(environ['SLACK_TOKEN'])
                   room(env.slack.room)
                   notifyAborted(true)
                   notifyFailure(true)
