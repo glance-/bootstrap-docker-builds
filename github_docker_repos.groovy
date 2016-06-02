c5ee2ecbce3753459b4a356b5662a806923a1636
Leif Johansson <leifj@sunet.se>
2016-06-02 14:40:38 +0200

    parse query

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -6,10 +6,12 @@ import static groovyx.net.http.ContentType.JSON
 def org = 'SUNET'
 def url = "https://api.github.com/"
 def next_path = "/orgs/${org}/repos"
+def next_query = [:]
+def api = new HTTPBuilder(url)
 while (next_path != null) {
-  def api = new HTTPBuilder(url)
   api.request(GET,JSON) { req ->
     uri.path = next_path
+    uri.query = next_query
     headers.'User-Agent' = 'Mozilla/5.0'
 
     response.success = { resp, reader ->
@@ -20,11 +22,17 @@ while (next_path != null) {
     next_path = null
     resp.headers.'Link'.split(',').each {
        it = it.trim()
-       out.println(it)
-       out.println("---")
        def m = (it =~ /<https:\/\/api.github.com([^>]+)>; rel="next"/)
        if (m.matches()) {
-          next_path = m[0][1]
+          def a = m[0][1].split('?')
+          next_path = a[0]
+          next_query = [:]
+          if (a.length == 2) {
+             a[1].split('&').each {
+                def av = it.split('=')
+                next_query[av[0]] = av[1]
+             }
+          }
        }
        out.println(next_path)
     }
