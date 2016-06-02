2cc894cd80cd397437b471f6b4075889518ea4a5
Leif Johansson <leifj@sunet.se>
2016-06-02 08:42:12 +0200

    dump link

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -21,10 +21,11 @@ while (next_path != null) {
     resp.headers.'Link'.split(',').each {
        out.println(it)
        out.println("---")
-       def m = (it =~ /<https:\/\/api.github.com(\s+)>;\S+rel=\"next\"/)
+       def m = (it =~ /<https:\/\/api.github.com([^>]+)>;\S+rel=\"next\"/)
        if (m.matches()) {
           next_path = m.group(1)
        }
+       out.println(next_path)
     }
 
     repos.each {
