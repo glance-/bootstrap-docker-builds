028e6ea528ca0cda1a51860ff1e82ca908c4da0c
Anton Lundin <anton.lundin@umu.se>
2019-09-05 14:37:28 +0200

    Switch yaml library to one that handles null
    snakeyaml seems to be the defacto yaml in groovy, and it handles null
    correctly so switch to that one.

--- github_docker_repos.groovy
+++ github_docker_repos.groovy
@@ -1,6 +1,6 @@
 // vim: ts=4 sts=4 sw=4 et
 import groovy.json.JsonSlurper
-import org.ho.yaml.Yaml
+import org.yaml.snakeyaml.Yaml
 // When we're at groovy 3.0
 //import groovy.yaml.YamlSlurper
 import java.io.FileNotFoundException
@@ -229,8 +229,7 @@ def add_job(env) {
                     out.println("${env.full_name} using trigger github push")
                     githubPush()
                 }
-                // Workaround org.ho.yaml.Yaml bug that resolvs null to the string null
-                if (env.triggers.cron != null && env.triggers.cron != "null") {
+                if (env.triggers.cron != null) {
                     out.println("${env.full_name} using trigger cron: ${env.triggers.cron}")
                     cron(env.triggers.cron)
                 }
