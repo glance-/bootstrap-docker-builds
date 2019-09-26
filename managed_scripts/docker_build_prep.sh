de03ca262de2e148753e735ba71a9d07f04b1f6c
Johan Lundberg <lundberg@sunet.se>
2019-09-26 17:46:03 +0200

    Add --always flag to git describe so it won't fail

--- managed_scripts/docker_build_prep.sh
+++ managed_scripts/docker_build_prep.sh
@@ -10,7 +10,7 @@ pwd
 
 # revision.txt is updated on every build,
 # used to enforce building of some but not all layers in Dockerfiles
-test -d docker && (git describe > docker/revision.txt; git log -n 1 >> docker/revision.txt)
+test -d docker && (git describe --always > docker/revision.txt; git log -n 1 >> docker/revision.txt)
 
 GIT_BRANCH=$(echo ${GIT_BRANCH} | sed -e 's/origin./git-/')
 export GIT_BRANCH
